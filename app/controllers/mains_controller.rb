e388b06a6de03f86dff1dec1df65492ccf9fff46
Wendy <wendy.d.ra@gmail.com>
2015-05-05 15:32:25 -0700

    Added accordian form submission for ingredients.

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -23,5 +23,9 @@ class MainsController < ApplicationController
 
   def test
     @test = params[:output]
+    @category = params[:category]
+
+    # Queries in Waiting
+    # User.find(session[:user_id]).meals.create(symptom_id: params[:symptom]).ingredients.create(name: params[:ingredient], category_id: params[:category_id])
   end
 end
