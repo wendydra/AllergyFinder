b8d4bec47f3dede56b782b50cb2042f5cb5712c3
Garrett <Garrett56x@gmail.com>
2015-05-08 10:22:24 -0700

    added triggers data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,8 @@ class SessionsController < ApplicationController
   	#  references the function we made in user.rb
   	user = User.authenticate(params[:session][:email], params[:session][:password])
 	if user.nil?
-		flash[:error] = user.errors.full_messages
-		# redirect_to '/users'
+		flash[:errors] = ['Incorrect login information.']
+		redirect_to '/users'
 	else
 		session[:user_id] = user.id
 		redirect_to '/mains'
